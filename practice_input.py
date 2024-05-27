b6ad3ac26521cebdaa2d2f734b0ee5f7d4bccd28
YechengYueEddy <yy4908@nyu.edu>
2024-05-27 16:21:36 +0800

    finished instructions

--- practice_input.py
+++ practice_input.py
@@ -11,6 +11,8 @@ def get_favorite_vegetable():
     where X is replaced with the user's favorite vegetable.
     """
     # write your code here.
+    fav_veg = input("What is your favorite vegetable? ")
+    print("Interesting! I also love " + fav_veg + "!")
 
 
 def get_favorite_number():
@@ -20,6 +22,8 @@ def get_favorite_number():
     where X is replaced with the user's favorite number.
     """
     # write your code here.
+    fav_num = input("What is your favorite number? ")
+    print("Interesting! I also love " + fav_num + "!")
 
 
 def get_name_and_zodiac_sign():
@@ -30,6 +34,9 @@ def get_name_and_zodiac_sign():
     where X and Y are replaced by the user's name and zodiac sign, respectively.
     """
     # write your code here.
+    user_name = input("What is your name? ")
+    user_zodiac = input("What is your zodiac sign? ")
+    print("Interesting! My name is also " + user_name + ", and I'm also a " + user_zodiac + "!")
 
 
 def get_name_and_age():
@@ -40,3 +47,7 @@ def get_name_and_age():
     where X and Y are replaced by the user's name and age, respectively.
     """
     # write your code here.
+    user_name = input("What is your name? ")
+    user_age = input("What is your age? ")
+    print("Interesting! My name is also " + user_name + ", and I'm also " + user_age + " years old!")
+
